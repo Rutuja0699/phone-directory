0bb8b14ffebd92922d235bb250e6716aeb72fe9b
Rutuja0699 <rutujapatil0699@gmail.com>
2022-02-01 12:54:15 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
+import Header from "./Header";
 function App() {
   return (
-    <><div className="header">
-      Phone Directory
-    </div><button> Add </button><div>
+    <><Header/>
+    <button> Add </button><div>
         <span> Name </span><br />
         <span> Phone </span>
       </div></>
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+// functional component
+const Header = function () {
+    return (
+        <div className="header">
+            Phone Directory
+        </div>
+    )
+    //}
+
+    // class component
+{/*class Header extends Component
+render() {
+        return(
+        <div className = "header" >
+                Phone Directory
+        </div >
+
+    )
+}*/}
+}
+export default Header;
