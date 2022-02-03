61dd2c46b03be934139920cd0111a818746eb8df
Rutuja0699 <rutujapatil0699@gmail.com>
2022-02-03 13:34:02 +0530

    Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
 import Header from "./Header";
 import './App.css';
 import { render } from "@testing-library/react";
+import { Component } from "react/cjs/react.production.min";
+import './common.css'
 
-function App() {
-  render()
-    let subscribers = [
-      {
-        name: 'Rutuja',
-        phone: '8888888888'
-      },
-      {
-        name: 'Mihir',
-        phone: '5555555555'
-      }
-    ]
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    };
+  }
 
-    
-    return (
-   // <><Header/>
-   // <button> Add </button><div>
-    //    <span> Name </span><br />
-     //   <span> Phone </span>
-       <> <Header heading='Phone Directory'/>
-        <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
+  render(){
+  // let subscribers = [
+  //   {
+  //     name: 'Rutuja',
+  //     phone: '8888888888'
+  //   },
+  //   {
+  //     name: 'Mihir',
+  //     phone: '5555555555'
+  //   }
+  //];
+  return(
+      <><Header heading='Phone Directory' /><div className="component-body-container">
+        <button className="custom-btn add-btn">Add</button>
 
-          <div className="grid-container heading-container">
-            <span className="grid-item name-heading">Name</span>
-            <span className="grid-item phone-heading">Phone</span>
-           
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
 
-          </div>
 
-          {
-            subscribers.map(sub => {
-              return <div className="grid-container">
-                <span className="grid-item">{sub.name}</span>
-                <span className="grid-item">{sub.phone}</span>
-                <button className="custom-btn delete-btn">Delete</button>
-                
-                </div>
-            })
-          }
+        </div>
 
-              </div>
-     </>
+        {this.state.subscribersListToShow.map(sub => {
+          return <div className="grid-container">
+            <span className="grid-item">{sub.name}</span>
+            <span className="grid-item">{sub.phone}</span>
+            <button className="custom-btn delete-btn">Delete</button>
 
-        );
+          </div>;
+        })}
+
+      </div></>
+      
+
+    );
+  }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './common.css'
 
 
 ReactDOM.render(
-  <AddSubscriber/>,
+  <App/>,
   document.getElementById('root')
 );
 
